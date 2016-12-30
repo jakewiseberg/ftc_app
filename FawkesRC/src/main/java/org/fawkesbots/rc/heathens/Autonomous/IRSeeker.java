a4e05c468c0b2493319e97726cf56d90846f1db6
Shyamp99 <shyamp99@gmail.com>
2016-12-29 19:15:31 -0500

    Add files via upload

---
+++ FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Autonomous/IRSeeker.java
@@ -0,0 +1,34 @@
+package org.fawkesbots.rc.heathens.Autonomous;
+
+        import com.qualcomm.robotcore.hardware.IrSeekerSensor;
+        import org.fawkesbots.rc.heathens.Hardware.HardwareTank;
+        import org.fawkesbots.rc.vendetta.Auton;
+
+
+/**
+ * Created by shyam on 12/14/2016.
+ */
+
+
+public class IRSeeker  extends Auton{
+    public void runOpMode() throws InterruptedException {
+        IrSeekerSensor seeker;
+        seeker= hardwareMap.irSeekerSensor.get("sensor ir");
+        waitForStart();
+        while(opModeIsActive()){
+            if(seeker.signalDetected()){
+                telemetry.addData("Angle", seeker.getAngle());
+                telemetry.addData("Strength", seeker.getStrength());
+            }
+            else{
+                telemetry.addData("Seeker", "Signal Lost");
+            }
+            telemetry.update();
+        }
+    }
+
+}
+
+
+
+
