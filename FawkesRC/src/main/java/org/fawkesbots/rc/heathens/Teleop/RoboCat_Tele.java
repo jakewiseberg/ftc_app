e4e8d2123236c2f3d3acd30e7e8fb1ab7f26ca13
KrishSeth <kskrish99@gmail.com>
2017-01-02 14:49:07 -0500

    Add files via upload

--- FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Teleop/RoboCat_Tele.java
+++ FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Teleop/RoboCat_Tele.java
@@ -8,6 +8,7 @@ import org.fawkesbots.rc.heathens.Hardware.HardwareCollector;
 import org.fawkesbots.rc.heathens.Hardware.HardwareFlicker;
 import org.fawkesbots.rc.heathens.Hardware.HardwareLauncher;
 import org.fawkesbots.rc.heathens.Hardware.HardwareLift;
+import org.fawkesbots.rc.heathens.Hardware.HardwareLifter;
 import org.fawkesbots.rc.heathens.Hardware.HardwareMecanum;
 
 import java.sql.DriverManager;
@@ -31,6 +32,9 @@ public class RoboCat_Tele extends OpMode {
 
     Gamepad 2:
     - launcher (right trigger)
+    -collector (left joy stick)
+    -flicker (right and left bumpers)
+    -Scissor Lifts  (right joystick)
 
      */
 
@@ -39,6 +43,7 @@ public class RoboCat_Tele extends OpMode {
     public HardwareLauncher Launcher;
     public HardwareFlicker Flicker;
     public HardwareLift Lift;
+    public HardwareLifter Lifter;
 
     public void init() {
         Drive = new HardwareMecanum(hardwareMap);
@@ -46,17 +51,21 @@ public class RoboCat_Tele extends OpMode {
         Launcher = new HardwareLauncher(hardwareMap);
         Flicker = new HardwareFlicker(hardwareMap);
         Lift = new HardwareLift(hardwareMap);
+        Lifter = new HardwareLifter(hardwareMap);
         Drive.hardwareSetup();
         Sweeper.hardwareSetup();
         Launcher.hardwareSetup();
         Flicker.hardwareSetup();
         Lift.hardwareSetup();
+        Lifter.hardwareSetup();
     }
     public void loop() {
         Drive.mecanum(-1*gamepad1.left_stick_x,-1*gamepad1.left_stick_y,gamepad1.right_stick_x);
         Sweeper.collect(gamepad2.left_stick_y);
         Launcher.fire((gamepad2.right_trigger>0.3)?-0.78f:(gamepad2.left_trigger>0.3)?0.78f:0);
         Flicker.flick(gamepad2.left_bumper || gamepad2.right_bumper);
-        Lift.rise(gamepad2.right_stick_y);
+        Lift.rise(-1*gamepad2.right_stick_y);
+        Lifter.rise(-1*gamepad2.right_stick_y);
+
     }
 }
