fbd7281cd44e26a5e20111e26a480a85d5552bfe
KrishSeth <kskrish99@gmail.com>
2017-01-05 21:45:24 -0500

    Add files via upload

--- FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Autonomous/IRSeeker.java
+++ FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Autonomous/IRSeeker.java
@@ -1,8 +1,20 @@
 package org.fawkesbots.rc.heathens.Autonomous;
 
-        import com.qualcomm.robotcore.hardware.IrSeekerSensor;
-        import org.fawkesbots.rc.heathens.Hardware.HardwareTank;
-        import org.fawkesbots.rc.vendetta.Auton;
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.IrSeekerSensor;
+
+import org.fawkesbots.rc.heathens.Hardware.HardwareEncoder;
+import org.fawkesbots.rc.heathens.Hardware.HardwareMecanum;
+import org.fawkesbots.rc.heathens.Hardware.HardwareMecanumWithEncoders;
+import org.fawkesbots.rc.heathens.Hardware.HardwareTank;
+import org.fawkesbots.rc.vendetta.Auton;
+
+@Autonomous(
+        name = "IRseeker",
+        group = "testing"
+)
 
 
 /**
@@ -10,24 +22,32 @@ package org.fawkesbots.rc.heathens.Autonomous;
  */
 
 
-public class IRSeeker  extends Auton{
+public class IRSeeker extends Auton {
+    public HardwareMecanum Drive;
+
     public void runOpMode() throws InterruptedException {
         IrSeekerSensor seeker;
-        seeker= hardwareMap.irSeekerSensor.get("sensor ir");
+        seeker = hardwareMap.irSeekerSensor.get("sensor ir");
         waitForStart();
-        while(opModeIsActive()){
-            if(seeker.signalDetected()){
-                telemetry.addData("Angle", seeker.getAngle());
-                telemetry.addData("Strength", seeker.getStrength());
+        while (true) {
+            
+            if (seeker.getStrength() >= 155)
+                Drive.mecanum(0, 0, 0.78f);
+             else if ((float) (seeker.getStrength()) < 155) {
+                Drive.mecanum(0, 0, -1 * 0.78f);
+            } else {
+                Log.e("Autonomous", "runOpMode: ");
             }
-            else{
-                telemetry.addData("Seeker", "Signal Lost");
-            }
-            telemetry.update();
         }
     }
 
+
 }
+
+
+
+
+
 
 
 
