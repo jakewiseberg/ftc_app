2fed33a7dd6f8aa8d9f8fbb746cb9c7040e985af
KrishSeth <kskrish99@gmail.com>
2017-02-10 12:21:43 -0500

    Add files via upload

--- FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Hardware/HardwareLift2.java
+++ FawkesRC/src/main/java/org/fawkesbots/rc/heathens/Hardware/HardwareLift2.java
@@ -1,5 +1,15 @@
 package org.fawkesbots.rc.heathens.Hardware;
 
+/**
+ * Created by krishenseth on 2/9/17.
+ */
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.fawkesbots.rc.vendetta.FawkesMotor;
+import org.fawkesbots.rc.vendetta.Sentinel;
+
+
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.fawkesbots.rc.heathens.DefSentinel;
@@ -8,24 +18,24 @@ import org.fawkesbots.rc.vendetta.Sentinel;
 
 
 
-public class HardwareLift extends Sentinel {
+public class HardwareLift2 extends Sentinel {
 
-    public FawkesMotor lift_uno;
+   // public FawkesMotor lift_uno;
     public FawkesMotor lift_dos;
 
-    public HardwareLift(HardwareMap a) {
+    public HardwareLift2(HardwareMap a) {
         super(a);
     }
 
     public boolean hardwareSetup() {
-        lift_uno=retrieveMotor("lift");
+        //lift_uno=retrieveMotor("lift");
         lift_dos=retrieveMotor("lifter");
         return true;
     }
 
     public boolean rise(float speed) {
-        lift_uno.power(0.7f*speed);
-        lift_dos.power(0.7f*speed);
+        //lift_uno.power(0.7f*speed);
+        lift_dos.power(speed);
         return true;
     }
 
